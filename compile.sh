0f57d74045b6ba96c9bd8b485fd909637895fc2e
Eduardo Kraus <kraus@MacNovo-do-kraus.local>
2018-06-25 16:50:12 -0300

    PHPDocs style problems

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-find ../ -name ".DS_Store"   -depth -exec rm -v {} \;
-
-rm -f media_videofront.zip
-
-zip -r ./media_videofront.zip ../videofront/ --exclude=*sh --exclude=*map --exclude=*scss --exclude=*.git* --exclude=.gitignore
-
-open ./
